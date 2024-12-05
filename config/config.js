5e72ec5343bce2a360a359eac9e394a6738e7fd1
DanielSoto99 <155490606+DanielSoto99@users.noreply.github.com>
2024-12-05 12:10:04 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,5 +2,9 @@
 require('dotenv').config();
 
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'hola', // Asegúrate de usar la variable de entorno
+  jwtSecret: process.env.JWT_SECRET || 'hola',
+   dbName : process.env.DB_NAME,
+  dbPort : process.env.DB_PORT,
+  dbPassword : process.env.DB_PASSWORD ,// Asegúrate de usar la variable de entorno
+  dbUsername : process.env.DB_USERNAME
 };
